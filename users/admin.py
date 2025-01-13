223bcc2ef5ec58f7ff6163fb6201560866fd6db9
zyzka <zuziaswis@gmail.com>
2025-01-13 19:52:44 +0100

    hashing password when adding via admin

--- users/admin.py
+++ users/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 from users.models import User, Coach
+from django.contrib.auth.admin import UserAdmin
 
-admin.site.register(User)
+class CustomUserAdmin(UserAdmin):
+    def save_model(self, request, obj, form, change):
+        if not change and form.cleaned_data.get('password'):
+            obj.set_password(form.cleaned_data['password'])
+        super().save_model(request, obj, form, change)
+    
+admin.site.register(User, CustomUserAdmin)
 admin.site.register(Coach)
