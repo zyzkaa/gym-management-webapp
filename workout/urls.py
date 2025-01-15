c0f655dbacab0195de1d725a6a5355f042f2b32d
zyzka <zuziaswis@gmail.com>
2025-01-15 20:48:04 +0100

    app names

--- workout/urls.py
+++ workout/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'workout'
+
 urlpatterns = [
     path('add/', views.add_workout, name='add_workout'),
     path('schedule/', views.schedule, name='schedule'),
