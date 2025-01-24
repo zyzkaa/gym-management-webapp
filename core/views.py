4ca0504d7d0114cdb6f8a935fc804a3686bba705
zyzka <zuziaswis@gmail.com>
2025-01-24 21:11:01 +0100

    coaches page

--- core/views.py
+++ core/views.py
@@ -1,6 +1,4 @@
 from django.shortcuts import render
 
-
-
 def home(request):
-    return render(request, 'core/index.html')
+    return render(request, 'core/main.html')
--- users/admin.py
+++ users/admin.py
@@ -1,8 +1,20 @@
+from dataclasses import fields
+from operator import indexOf
+
 from django.contrib import admin
 from users.models import User, Coach
 from django.contrib.auth.admin import UserAdmin
 
 class CustomUserAdmin(UserAdmin):
+    fieldsets = UserAdmin.fieldsets + (
+        (
+            'Other',
+            {
+                'fields': ['is_coach', 'gender', 'profile_picture']
+            }
+        ),
+    )
+
     def save_model(self, request, obj, form, change):
         if not change and form.cleaned_data.get('password'):
             obj.set_password(form.cleaned_data['password'])
